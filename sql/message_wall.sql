78b4c5e084281b61c0e80866aa9b0fa34e856373
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-12-24 16:00:20 -0700

    Update message_wall.sql

--- sql/message_wall.sql
+++ sql/message_wall.sql
@@ -1,14 +1,12 @@
--- Table to store message wall threads
+-- Table to store message wall messages
 CREATE TABLE message_wall (
-	mw_thread_id INT NOT NULL PRIMARY KEY,
-	mw_parent_id INT NULL,
+	mw_username VARCHAR(255) NOT NULL,
 	mw_user_id INT NOT NULL,
+	mw_user_name VARCHAR(255) NOT NULL,
+	mw_message TEXT NOT NULL,
 	mw_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	mw_latest_rev_id INT NOT NULL
+	PRIMARY KEY (mw_username, mw_timestamp)
 );
 
--- Index on message wall thread user IDs
+-- Index on message wall user IDs
 CREATE INDEX message_wall_user_id_idx ON message_wall (mw_user_id);
-
--- Index on message wall thread parent IDs
-CREATE INDEX message_wall_parent_id_idx ON message_wall (mw_parent_id);
